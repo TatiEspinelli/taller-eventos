180c2ff0f3e80fc2a801f533c6dbb16ae26bf45c
Tati Espinelli <tatiespinelli@gmail.com>
2023-08-09 19:23:48 -0300

    solucion click

--- eventos.js
+++ eventos.js
@@ -1,7 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
     const divBoton = document.getElementById("divBoton");
-  
+    const saludarBtn = document.getElementById("saludarBtn");
+
     divBoton.addEventListener("click", function () {
-      alert("Hola! Soy el div");
+        alert("Hola! Soy el div");
     });
-  });
+
+    saludarBtn.addEventListener("click", function (event) {
+        alert("Hola!");
+        event.stopPropagation();
+    });
+});
